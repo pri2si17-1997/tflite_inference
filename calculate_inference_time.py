ef0ab90eca564d627a0d3057ed083238454a005f
Priyanshu Sinha <priyanshu_sinha@outlook.com>
2021-03-28 19:02:10 +0530

    Added sample images and weights file. Added script to compute time.
    Signed-off-by: Priyanshu Sinha <priyanshu_sinha@outlook.com>

---
+++ calculate_inference_time.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import pandas as pd
+import cv2
+import time
+import numpy as np
+import os
+import glob2
+import random
+import shutil
+
+random.seed(42)
+
+class_names = ['Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 'Nodule', 'Pneumonia', 'Pneumothorax', 'Consolidation', 'Edema', 'Emphysema', 'Fibrosis', 'Pleural_Thickening', 'Hernia']
+
+IMAGE_PATH = os.path.abspath('./test_images')
+WEIGHT_PATH = os.path.abspath('./weights/chexnet_kaggle_tflite_dynaic_quantization.tflite')
+images = sorted(glob2.glob(IMAGE_PATH + '/*.png'))
+print(f"Total number of Images : {len(images)}")
+
+def evaluate_timing():
+    total_time = 0
+    interpreter = tf.lite.Interpreter(model_path = WEIGHT_PATH)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()[0]
+    output_details = interpreter.get_output_details()[0]
+
+    for idx, img_item in enumerate(images):
+        img = cv2.imread(img_item)
+        img = cv2.resize(img, (224, 224)) / 255
+        test_image = np.expand_dims(img, axis=0).astype(input_details["dtype"])
+        interpreter.set_tensor(input_details['index'], test_image)
+        start_time = time.perf_counter()
+        interpreter.invoke()
+        end_time = time.perf_counter()
+        time_taken = end_time - start_time
+        total_time += time_taken
+
+    print(f"Total Time taken : {total_time} seconds.")
+    print(f"Average Time Taken : {total_time / len(images)} seconds")
+
+if __name__ == "__main__":
+    evaluate_timing()
+
